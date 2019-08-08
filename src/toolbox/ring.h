5a0b02b0b823368c2b9a6f7a6bb5d6076582ca9e
刘群 <qun.liu@sdxformula.com>
2019-08-08 18:10:37 +0800

    修改API函数参数类型ring_new(capacity)
    使用枚举型强制保证环形队列的结点总个数是2的N次方幂

--- src/toolbox/ring.h
+++ src/toolbox/ring.h
@@ -10,9 +10,38 @@
 #define RINGLIB_PRINT_VERSION() \
 	printf("Ring lib version=%.2f\n", RINGLIB_VERSION_NUMBER_X_Y / 1000.0f)
 
+#define RINGLIB_DEBUG_ALLOW_ALL /* 调试专用宏：统一开启全部调试开关 */
+
+#if defined(RINGLIB_DEBUG_ALLOW_ALL)
+#define RINGLIB_DEBUG_ALLOW_TINY_CAPACITY    /* 调试专用宏：允许设置特别小的环型队列容量 */
+#endif
+
+#if (!defined(RINGLIB_DEBUG_ALLOW_ALL) && RINGLIB_VER_X>=1)
+#define RINGLIB_IS_A_STABLE_VERSION
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
+
+
+enum RingCapacity {// 环形队列容量(可容纳结点的总个数)必须是2^N(例如128，256，512，1024等等)，这么规定是为了优化执行速度
+	/* 本环形队列的容量为固定常数值，并不支持自动扩容 */
+	RING_CAPACITY_FIXED_TO_128=128,
+	RING_CAPACITY_FIXED_TO_256=256,
+	RING_CAPACITY_FIXED_TO_512=512,
+	RING_CAPACITY_FIXED_TO_1024=1024,
+	RING_CAPACITY_FIXED_TO_2048=2048,
+	RING_CAPACITY_FIXED_TO_4096=4096,
+	RING_CAPACITY_FIXED_TO_8192=8192,
+#if RINGLIB_IS_A_STABLE_VERSION
+	RING_CAPACITY_FIXED_TO_DEFAULT_SIZE=4096,
+#elif defined(RINGLIB_DEBUG_ALLOW_TINY_CAPACITY)
+	/* 以下参数仅仅用于调试环境，未经测试不得在生产环境下启用！ */
+	RING_CAPACITY_FIXED_TO_DEFAULT_SIZE=2, // 测试专用
+	RING_CAPACITY_FIXED_TO_4=4, // 测试专用
+#endif
+};
 
 struct ring_ {
 #if defined(RING_ENABLE_LOCKFREE_FEATURE)
@@ -21,7 +50,7 @@ struct ring_ {
 	int consumer_head_idx;
 	int consumer_tail_idx;
 #endif
-	unsigned int max_ring_nodes;
+	enum RingCapacity cap;//最大容量
 	unsigned int head;
 	unsigned int tail;
 	struct node_ {
@@ -34,7 +63,7 @@ struct ring_ {
 typedef struct ring_ Ring;
 typedef struct node_ Node;
 
-Ring * ring_new(unsigned int how_many_nodes);// 环形队列结点总个数必须是2^N, 例如128，256，512，1024等等
+Ring * ring_new(enum RingCapacity fixed_max_capacity);// 环形队列容量必须是2^N, 例如128，256，512，1024等等。这样规定目的是为了提高执行效率
 void ring_free(Ring *ring);
 #define ring_free_gracefully(p) \
 	do { \
